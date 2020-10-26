90ddde6e42def852ff35d573559b319e82d6b8e8
bobkombat <rdhwnrmdhny@gmail.com>
2020-10-26 19:52:14 +0700

    - finish 3 heuristics

--- app/src/algorithm/helper/heuristic.js
+++ app/src/algorithm/helper/heuristic.js
@@ -17,8 +17,16 @@ export default function heuristicTypeChooser(
   }
 }
 
-function manhattanDistance(currentY, currentX, finishY, finishX) {}
+function manhattanDistance(currentY, currentX, finishY, finishX) {
+  return Math.abs(finishX - currentX) + Math.abs(finishY - currentY);
+}
 
-function euclideanDistance(currentY, currentX, finishY, finishX) {}
+function euclideanDistance(currentY, currentX, finishY, finishX) {
+  return Math.sqrt(
+    Math.pow(finishX - currentX, 2) + Math.pow(finishY - currentY, 2)
+  );
+}
 
-function chebyshevDistance(currentY, currentX, finishY, finishX) {}
+function chebyshevDistance(currentY, currentX, finishY, finishX) {
+  return Math.max(Math.abs(finishX - currentX), Math.abs(finishY, currentY));
+}
